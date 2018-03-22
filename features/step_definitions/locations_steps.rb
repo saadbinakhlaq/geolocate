8d3fc148f60d7f79a02361754f840f52ddc02779
Saad Bin Akhlaq <saad.18@gmail.com>
2018-03-22 08:33:53 +0100

    feature(locations): Add feature specs

--- features/step_definitions/locations_steps.rb
+++ features/step_definitions/locations_steps.rb
@@ -1,11 +1,14 @@
 When("I perform get request to the locations endpoint with address query params") do
-  pending
+  stub_request(:get, /maps.googleapis.com/).
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => { 'status' => 'OK', 'results' => [ { geometry: { location: { lat: 52, lng: 13}}}]}.to_json, :headers => {})
+  get '/locations?address=checkpoint+charlie'
 end
 
 Then("I should see a valid response") do
-  pending
+  last_response.status.should == 200
 end
 
 Then("The JSON response should be valid") do
-  pending
+  last_response.body.should == { latitude: 52, longitude: 13 }.to_json
 end
